6b49e718da9b9f8d0148bbdbc6aaa538c6e003fd
Gabriel Couture <gacou54@gmail.com>
2021-02-23 10:54:50 -0500

    Add tests for failing foward call

--- fortitude/orthanc_manager/tests/api/test_forward_call.py
+++ fortitude/orthanc_manager/tests/api/test_forward_call.py
@@ -5,16 +5,13 @@ from django.urls import reverse
 from parameterized import parameterized
 from rest_framework import status
 
+from .util import make_bad_dicom_data, make_dicom_data
 from ...models import OrthancServer
 
 NAME = 'NAME'
 ADDRESS = 'http://localhost:8042'
 USERNAME = 'orthanc'
 PASSWORD = 'orthanc'
-MODALITY = 'MODALITY'
-MODALITY_DATA = {'AET': 'ORTHANCC', 'Host': '127.0.0.1', 'Port': 2002}
-
-DICOM_FILE_PATH = './fortitude/orthanc_manager/tests/data/CT_small.dcm'
 
 
 class TestForwardCall(TestCase):
@@ -46,11 +43,11 @@ class TestForwardCall(TestCase):
         self.assertEqual(response.status_code, expected_status)
 
     @parameterized.expand([
-        ('instances/', status.HTTP_200_OK),
+        ('instances/', make_dicom_data(), status.HTTP_200_OK),
+        ('instances/', make_bad_dicom_data(), status.HTTP_400_BAD_REQUEST),
+        ('bad_route/', make_dicom_data(), status.HTTP_404_NOT_FOUND),
     ])
-    def test_forward_post_call_with_binary_data(self, route, expected_status):
-        dicom_data = self.given_dicom_data()
-
+    def test_forward_post_call_with_binary_data(self, route, dicom_data, expected_status):
         # The standard self.client.post force data serialization as json or a multipart message
         response = self.client.generic(
             method='POST',
@@ -61,36 +58,39 @@ class TestForwardCall(TestCase):
 
         self.assertEqual(response.status_code, expected_status)
 
-    @parameterized.expand([(status.HTTP_200_OK,)])
-    def test_forward_delete_call(self, expected_status):
-        instance_path = self.given_dicom_instance_in_orthanc()
+    @parameterized.expand([
+        ('instances/', status.HTTP_200_OK,),
+        ('bad_route/', status.HTTP_404_NOT_FOUND,),
+    ])
+    def test_forward_delete_call(self, route, expected_status):
+        instance_id = self.given_dicom_instance_in_orthanc()
 
         response = self.client.delete(
-            reverse('orthanc-forward-call', kwargs={'server_name': NAME, 'route': instance_path}),
+            reverse('orthanc-forward-call', kwargs={'server_name': NAME, 'route': f'{route}/{instance_id}'}),
         )
 
         self.assertEqual(response.status_code, expected_status)
 
-    @parameterized.expand([(status.HTTP_200_OK,)])
-    def test_forward_put_call(self, expected_status):
+    @parameterized.expand([
+        ('A_MODALITY', {'AET': 'AN_AET', 'Host': '127.0.0.1', 'Port': 2002}, status.HTTP_200_OK),
+        ('A_MODALITY', {'BAD_FIELD': 'AN_AET', 'Host': '127.0.0.1', 'Port': 2002}, status.HTTP_400_BAD_REQUEST),
+    ])
+    def test_forward_put_call(self, modality, modality_data, expected_status):
+        """Put /modalities/{modality_name} is used as test"""
         response = self.client.put(
             reverse('orthanc-forward-call', kwargs={
                 'server_name': NAME,
-                'route': f'modalities/{MODALITY}'
+                'route': f'modalities/{modality}'
             }),
-            data=MODALITY_DATA,
+            data=modality_data,
             content_type='application/json'
         )
 
         self.assertEqual(response.status_code, expected_status)
 
-    def given_dicom_data(self) -> bytes:
-        with open(DICOM_FILE_PATH, 'rb') as file:
-            return file.read()
-
     def given_dicom_instance_in_orthanc(self) -> str:
-        """Returns the DICOM objects id path in Orthanc"""
-        dicom_data = self.given_dicom_data()
+        """Returns the DICOM objects ID in Orthanc"""
+        dicom_data = make_dicom_data()
 
         # The standard self.client.post force data serialization as json or a multipart message
         response = self.client.generic(
@@ -99,11 +99,10 @@ class TestForwardCall(TestCase):
             data=dicom_data,
             content_type='application/dicom'
         )
-
         self.assertEqual(
             response.status_code,
             status.HTTP_200_OK,
             'Failed to upload DICOM file (required for the test).'
         )
 
-        return json.loads(response.data)['Path'][1:]
+        return json.loads(response.data)['ID']
