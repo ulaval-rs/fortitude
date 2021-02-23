a5d4503d4bd6aa098b20498f6bc88c25fe273a29
Gabriel Couture <gacou54@gmail.com>
2021-02-23 10:20:46 -0500

    Removing unused logging middleware

--- fortitude/settings.py
+++ fortitude/settings.py
@@ -41,7 +41,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'fortitude.users.middlewares.LoggingMiddleware',
 ]
 
 ROOT_URLCONF = 'fortitude.urls'
