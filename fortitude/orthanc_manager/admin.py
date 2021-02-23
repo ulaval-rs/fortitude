bc3b83709583741cfcda8f90ba9343ef846a1d90
Gabriel Couture <gacou54@gmail.com>
2021-02-23 10:33:34 -0500

    Improve admin panel for Orthanc servers

--- fortitude/orthanc_manager/admin.py
+++ fortitude/orthanc_manager/admin.py
@@ -2,4 +2,22 @@ from django.contrib import admin
 
 from .models import OrthancServer
 
-admin.site.register(OrthancServer)
+
+class OrthancServerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address')
+    fieldsets = (
+        ('Configuration', {
+            'fields': (
+                'name',
+                'address',
+                'has_credentials',
+                'username',
+                'password',
+            )
+        }),
+    )
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+admin.site.register(OrthancServer, OrthancServerAdmin)
